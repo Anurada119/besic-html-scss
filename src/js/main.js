3fb7eba6b41c787eb6f111420bb7d0c47a360d6c
Anurada Gunarathna <anurada119@gmail.com>
2017-09-17 22:45:29 +0530

    header

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,34 @@
 console.log('fu');
 console.log('fu too');
+
+$(document).ready(function() {
+
+    $('.search-icon').on('click', function() {
+        $('.fr-searchbar').slideToggle();
+    });
+
+    $('.burger.sidenav-toggle').on('click', function() {
+        $('#navbarSupportedContent').toggleClass('sidenav-open');
+    });
+
+    $('.close-sidnav').on('click', function() {
+        $('#navbarSupportedContent').removeClass('sidenav-open');
+    });
+
+    dropDown();
+});
+
+$(window).resize(function() {
+    dropDown();
+})
+
+function dropDown() {
+    var wWidth = $(window).width();
+    if (wWidth < 991) {
+        $('.dropdown-menu, .nav-item.dropdown').addClass('show');
+    } else {
+        $('.dropdown-menu, .nav-item.dropdown').removeClass('show');
+    }
+
+
+}
