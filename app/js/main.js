83b355e2400a64f6b411f88eebb33e170357a648
Anurada Gunarathna <anurada119@gmail.com>
2017-09-19 01:01:38 +0530

    sliders

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
-$(document).ready(function () {
-    
+$(document).ready(function() {
+
     sidenav();
     dropDown();
 
@@ -9,42 +9,45 @@ $(document).ready(function () {
         speed: 300,
         slidesToShow: 4,
         slidesToScroll: 4,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
+        arrows: false,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    arrows: false,
+                    slidesToScroll: 3,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
             }
-          },
-          {
-            breakpoint: 769,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
         ]
-      });
+    });
 
 });
 
@@ -74,4 +77,3 @@ function sidenav() {
         $('#navbarSupportedContent').removeClass('sidenav-open');
     });
 }
-
